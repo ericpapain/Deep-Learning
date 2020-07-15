d83415bff1789a3b0f8a31522101aa3889154bc2
Eric Papain <36469392+ericpapain@users.noreply.github.com>
2020-07-15 18:10:28 +0200

    Update Generateur de Gan.py

--- exemple de générateur de réseau de GANs/Generateur de Gan.py
+++ exemple de générateur de réseau de GANs/Generateur de Gan.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon jan  6 04:51:02 2020
+
+@author: eric
+"""
+
 from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
 from tensorflow.keras import layers
